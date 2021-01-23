7976424ef6991908833a0536e6c889653a29aee4
Jon Zubizarreta <jonzubi97@gmail.com>
2021-01-23 20:11:59 +0100

    Prueba creacion objetos para ver los cambios en GIT

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{    
+    public float boundX, boundY;
+    SpawnManager m_SpawnManager;
+    int round = 1;
+    int enemiesLeft;
+
+    void Awake()
+    {
+        m_SpawnManager = FindObjectOfType<SpawnManager>();
+
+        boundY = Camera.main.orthographicSize;    
+        boundX = boundY * Screen.width / Screen.height;
+    }
+
+    void Start()
+    {
+        StartRound();
+    }
+
+    public void EnemyDead ()
+    {
+        enemiesLeft--;
+        if (enemiesLeft == 0)
+        {
+            round++;
+            Invoke("StartRound", 2f);
+        }
+    }
+
+    void StartRound()
+    {
+        m_SpawnManager.spawnEnemies(round);
+        enemiesLeft = round;
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject m_circle;
+    GameManager m_GameManager;
+    void Awake()
+    {
+        m_GameManager = FindObjectOfType<GameManager>();
+    }
+
+    void InstantiateEnemy()
+    {
+        Vector3 randomPos = GetRandomPos();
+        Instantiate(m_circle, GetRandomPos(), m_circle.transform.rotation);
+    }
+
+    Vector3 GetRandomPos ()
+    {
+        float randomX, randomY;
+        randomX = Random.Range(-m_GameManager.boundX, m_GameManager.boundX);
+        randomY = Random.Range(-m_GameManager.boundY, m_GameManager.boundY);
+        return new Vector3(randomX, randomY, -1);
+    }
+
+    float GetRandomTime(float min, float max)
+    {
+        return Random.Range(min, max);
+    }
+
+    public void spawnEnemies (int round)
+    {
+        for (int i = 0; i < round; i++)
+        {
+            InstantiateEnemy();
+            Invoke("InstantiateGota", GetRandomTime(3f, 7f));
+        }
+    }
+}
