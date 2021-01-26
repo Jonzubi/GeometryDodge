32c8358cc2c84612c645721fab9fd9dc2caa0115
Jon Zubizarreta <jonzubi97@gmail.com>
2021-01-26 18:56:25 +0100

    Contador funciona para iniciar la ronda

--- Assets/Scripts/Canvas/CanvasManager.cs
+++ Assets/Scripts/Canvas/CanvasManager.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CanvasManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    GameObject m_roundInfoText, m_secondsText;
+
+    public void SetRoundInfoText(string text)
     {
-        
+        m_roundInfoText.GetComponent<Text>().text = text;
     }
 
-    // Update is called once per frame
-    void Update()
+        public void SetSecondsInfoText(string text)
     {
-        
+        m_secondsText.GetComponent<Text>().text = text;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,13 +6,15 @@ public class GameManager : MonoBehaviour
 {    
     [HideInInspector]
     public float leftBoundX, rightBoundX, topBoundY, bottomBoundY;
-    SpawnManager m_SpawnManager;
+    SpawnManager m_spawnManager;
+    CanvasManager m_canvasManager;
     int round = 1;
     int enemiesLeft;
 
     void Awake()
     {
-        m_SpawnManager = FindObjectOfType<SpawnManager>();
+        m_spawnManager = FindObjectOfType<SpawnManager>();
+        m_canvasManager = FindObjectOfType<CanvasManager>();
 
         float auxBoundY = Camera.main.orthographicSize;    
         float auxBoundX = auxBoundY * Screen.width / Screen.height;
@@ -39,7 +41,19 @@ public class GameManager : MonoBehaviour
 
     void StartRound()
     {
-        m_SpawnManager.spawnEnemies(round);
+        m_canvasManager.SetRoundInfoText($"Round {round}");
+        StartCoroutine(CountDownAndStart());
+    }
+
+    IEnumerator CountDownAndStart()
+    {
+        for (int i = 3; i > 0; i--)
+        {
+            m_canvasManager.SetSecondsInfoText($"{i}");
+            yield return new WaitForSeconds(1);
+        }
+        m_canvasManager.SetSecondsInfoText("");
+        m_spawnManager.spawnEnemies(round);
         enemiesLeft = round;
     }
 }
