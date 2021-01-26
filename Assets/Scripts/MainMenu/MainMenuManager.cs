de0e85bfe23a4ce0e22cebb9aa396b6d19826594
Jon Zubizarreta <jonzubi97@gmail.com>
2021-01-26 23:00:38 +0100

    MainMenu simple hecho

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void PlayBtnClick()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitBtnClick()
+    {
+        Application.Quit();
+    }
+}
