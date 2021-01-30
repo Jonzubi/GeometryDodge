68c76b0ead39132d8c62d975ad933b83f88ced81
Jon Zubizarreta <jonzubi97@gmail.com>
2021-01-30 19:03:19 +0100

    Se crea y se guardan los datos en userData.json

---
+++ Assets/DataStructs/UserData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class UserData
+{
+    public int level;
+    public int totalXP;
+    public int totalCoins;
+    public int unlockedSlots;
+    public List<Item> items;
+    public int maxInventory;
+
+    public UserData()
+    {
+        level = 1;
+        totalXP = 0;
+        totalCoins = 0;
+        unlockedSlots = 1;
+        items = new List<Item>();
+        maxInventory = 10;
+    }
+}
+
+[System.Serializable]
+public class Item
+{
+    public int id;
+    public string name;
+    public int itemAmount;
+}
--- Assets/Scripts/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -1,10 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public GameObject txtLevel, txtCoins;
+    void Awake()
+    {
+        // Crear o leer el archivo userData
+        if (!File.Exists($"{Application.persistentDataPath}/UserData.json"))
+        {
+            UserData userData = new UserData();
+            string userDataJson = JsonUtility.ToJson(userData);
+            File.WriteAllText($"{Application.persistentDataPath}/UserData.json", userDataJson);
+            UserDataToCanvas(userData);
+        }
+        else
+        {
+            string userDataJson = File.ReadAllText($"{Application.persistentDataPath}/UserData.json");
+            UserData userData = JsonUtility.FromJson<UserData>(userDataJson);
+            UserDataToCanvas(userData);
+        }
+    }
+
+    void UserDataToCanvas(UserData userData)
+    {
+        txtLevel.GetComponent<Text>().text = $"Level: {userData.level}";
+        txtCoins.GetComponent<Text>().text = $"{userData.totalCoins}";
+    }
     public void PlayBtnClick()
     {
         SceneManager.LoadScene("GameScene");
