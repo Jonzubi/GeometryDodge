d358502f06a938c8bfd232eb4826c2d0ae335772
Jon Zubizarreta <jonzubi97@gmail.com>
2021-01-30 20:24:22 +0100

    Cuando se recogen las monedas aparece en la pantalla

---
+++ Assets/Scripts/HUD/CoinCollectorController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCollectorController : MonoBehaviour
+{
+    Text txtCoins;
+
+    void Awake()
+    {
+        txtCoins = GetComponent<Text>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txtCoins.text = GameDataCollector.m_coinsReceived.ToString();
+    }
+}
