3f915eab5b93b4c9f60add40c45471284ce44bf7
Jon Zubizarreta <jonzubi97@gmail.com>
2021-02-10 21:57:50 +0100

    Fix variable name

--- Assets/Scripts/HUD/SlotsController.cs
+++ Assets/Scripts/HUD/SlotsController.cs
@@ -9,10 +9,10 @@ public class SlotsController : MonoBehaviour
     
     void Awake()
     {
-        int unlockedSlosts = UserDataKeeper.userData.unlockedSlots;
+        int unlockedSlots = UserDataKeeper.userData.unlockedSlots;
 
         // TODO: Por el momento solo escondere las imagenes, en el futuro habria que cambioar las imagenes del candado por el objeto que haya metido el usuario
-        for(int i=0; i < unlockedSlosts;i++)
+        for(int i=0; i < unlockedSlots;i++)
         {
             Image[] images = slots[i].GetComponentsInChildren<Image>();
             foreach (Image image in images)
