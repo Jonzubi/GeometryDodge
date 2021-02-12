c7951cde5ba1dabbe377ce92c955faa19ee7503f
Jon Zubizarreta <jonzubi97@gmail.com>
2021-02-12 21:57:46 +0100

    PlayerController paquetes innecesarios eliminados

--- Assets/Scripts/GameScene/Player/PlayerController.cs
+++ Assets/Scripts/GameScene/Player/PlayerController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(InventoryController))]
 public class PlayerController : MonoBehaviour
