883f11870f6c67fed2e82889e94200715a999406
Jon Zubizarreta <jonzubi97@gmail.com>
2021-02-17 22:04:34 +0100

    Rendimiento mejorado al recoger SpriteRenderer en CoinController

--- Assets/Scripts/GameScene/Coin/CoinController.cs
+++ Assets/Scripts/GameScene/Coin/CoinController.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
+    SpriteRenderer m_spriteRenderer;
+
+    void Awake()
+    {
+        m_spriteRenderer = GetComponent<SpriteRenderer>();    
+    }
     public IEnumerator StartDisappearing(int timeLeft)
     {
-        Color auxColor = GetComponent<SpriteRenderer>().color;
+        Color auxColor = m_spriteRenderer.color;
         for (; timeLeft > 0; timeLeft--)
         {            
-            GetComponent<SpriteRenderer>().color = new Color(auxColor.r, auxColor.g, auxColor.b, 0f);
+            m_spriteRenderer.color = new Color(auxColor.r, auxColor.g, auxColor.b, 0f);
             yield return new WaitForSeconds(0.25f);
-            GetComponent<SpriteRenderer>().color = new Color(auxColor.r, auxColor.g, auxColor.b, 1f);
+            m_spriteRenderer.color = new Color(auxColor.r, auxColor.g, auxColor.b, 1f);
             yield return new WaitForSeconds(0.25f);
-            GetComponent<SpriteRenderer>().color = new Color(auxColor.r, auxColor.g, auxColor.b, 0f);
+            m_spriteRenderer.color = new Color(auxColor.r, auxColor.g, auxColor.b, 0f);
             yield return new WaitForSeconds(0.25f);
-            GetComponent<SpriteRenderer>().color = new Color(auxColor.r, auxColor.g, auxColor.b, 1f);
+            m_spriteRenderer.color = new Color(auxColor.r, auxColor.g, auxColor.b, 1f);
             yield return new WaitForSeconds(0.25f);
         }
         Destroy(gameObject);
