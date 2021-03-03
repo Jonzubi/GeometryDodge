0783bd4205d85a5411ff5d2f568b6b47162d2f97
Jon Zubizarreta <jonzubi97@gmail.com>
2021-03-03 22:17:22 +0100

    Las imagenes se ajustan respecto a su tamaño original en los Slots

--- Assets/Scripts/GameScene/HUD/SlotsController.cs
+++ Assets/Scripts/GameScene/HUD/SlotsController.cs
@@ -34,6 +34,7 @@ public class SlotsController : MonoBehaviour
                 {
                     if (i > items.Count - 1)
                     {
+                        image.gameObject.transform.parent.GetChild(1).gameObject.SetActive(true); // Activamos el texto del "Empty"
                         image.gameObject.SetActive(false); // En este punto tiene vacio el inventario
                         GameObject text = image.gameObject.transform.parent.GetChild(image.gameObject.transform.parent.childCount - 1).gameObject;
                         text.SetActive(false);
@@ -41,6 +42,14 @@ public class SlotsController : MonoBehaviour
                     else
                     {
                         image.sprite = itemSprites[(int)items[i].id]; // Cargar la imagen del item
+                        image.gameObject.transform.parent.GetChild(1).gameObject.SetActive(false); // Desactivamos el texto del "Empty"
+
+                        // Ajustar la imagen al tamaño de la caja
+                        Vector2 nativeSpriteSize = image.sprite.rect.size;
+                        RectTransform auxRect = image.gameObject.GetComponent<RectTransform>();
+                        float relation = 150 / nativeSpriteSize.y; // 150 es la altura que quiero que tenga la imagen siempre
+                        Vector2 auxSizeDelta = auxRect.sizeDelta;
+                        auxRect.sizeDelta = new Vector2(nativeSpriteSize.x * relation, 150);
                         image.gameObject.SetActive(true);
 
                         GameObject text = image.gameObject.transform.parent.GetChild(image.gameObject.transform.parent.childCount - 1).gameObject;
