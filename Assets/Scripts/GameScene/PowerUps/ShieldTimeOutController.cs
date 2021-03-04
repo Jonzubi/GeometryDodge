ca06cfece1ff37fc27e4182d3a43025c0f941047
Jon Zubizarreta <jonzubi97@gmail.com>
2021-03-04 21:47:57 +0100

    El shield tiene un timeout y desaparece

---
+++ Assets/Scripts/GameScene/PowerUps/ShieldTimeOutController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldTimeOutController : MonoBehaviour
+{
+    public float m_shieldTimeOut = 5;
+    SpriteRenderer m_spriteRenderer;
+
+    void Awake()
+    {
+        m_spriteRenderer = GetComponent<SpriteRenderer>();    
+    }
+
+    void Start()
+    {
+        StartCoroutine(StartDisappearing(3));
+    }
+    IEnumerator StartDisappearing(int timeLeft)
+    {
+        Color auxColor = m_spriteRenderer.color;
+        yield return new WaitForSeconds(m_shieldTimeOut - timeLeft);
+        for (; timeLeft > 0; timeLeft--)
+        {            
+            m_spriteRenderer.color = new Color(auxColor.r, auxColor.g, auxColor.b, 0f);
+            yield return new WaitForSeconds(0.25f);
+            m_spriteRenderer.color = new Color(auxColor.r, auxColor.g, auxColor.b, 1f);
+            yield return new WaitForSeconds(0.25f);
+            m_spriteRenderer.color = new Color(auxColor.r, auxColor.g, auxColor.b, 0f);
+            yield return new WaitForSeconds(0.25f);
+            m_spriteRenderer.color = new Color(auxColor.r, auxColor.g, auxColor.b, 1f);
+            yield return new WaitForSeconds(0.25f);
+        }
+        Destroy(gameObject);
+    }
+}
