b4eb318332e1a41c6ee16f1c302b90cc1f2ba55e
Jon Zubizarreta <jonzubi97@gmail.com>
2021-06-12 20:56:36 +0200

    AddMoney creado para modo debug

---
+++ Assets/Scripts/MainMenuScene/AddMoneyDebugMode.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AddMoneyDebugMode : MonoBehaviour
+{
+    public int m_money;
+
+    public void Exec()
+    {
+        UserDataKeeper.userData.totalCoins += m_money;
+        UserDataKeeper.SaveUserData();
+    }
+}
