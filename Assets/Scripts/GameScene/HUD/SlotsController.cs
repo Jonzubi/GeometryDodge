70fb44195deeb8b1f3a511c516193f12c6808056
Jon Zubizarreta <jonzubi97@gmail.com>
2021-07-08 21:49:22 +0200

    Los textos de los slots es TMPro

--- Assets/Scripts/GameScene/HUD/SlotsController.cs
+++ Assets/Scripts/GameScene/HUD/SlotsController.cs
@@ -23,37 +23,31 @@ public class SlotsController : MonoBehaviour
         }
         for(int i=0; i < unlockedSlots;i++)
         {
-            Image[] images = slots[i].GetComponentsInChildren<Image>(true);
-            foreach (Image image in images)
+            Image image = CommonFunctions.GetChildByName(slots[i], "SlotImage").GetComponent<Image>();
+            if (i > items.Count - 1)
             {
-                if (image.gameObject.name == "SlotImage")
-                {
-                    if (i > items.Count - 1)
-                    {
-                        image.gameObject.transform.parent.GetChild(1).gameObject.SetActive(true); // Activamos el texto del "Empty"
-                        image.gameObject.SetActive(false); // En este punto tiene vacio el inventario
-                        GameObject text = image.gameObject.transform.parent.GetChild(image.gameObject.transform.parent.childCount - 1).gameObject;
-                        text.SetActive(false);
-                    }
-                    else
-                    {
-                        image.sprite = ImageLoader.GetItem((int)items[i].id); // Cargar la imagen del item
-                        image.gameObject.transform.parent.GetChild(1).gameObject.SetActive(false); // Desactivamos el texto del "Empty"
+                CommonFunctions.GetChildByName(slots[i], "EmptyText").SetActive(true);
+                image.gameObject.SetActive(false); // En este punto tiene vacio el inventario
+                GameObject text = image.gameObject.transform.parent.GetChild(image.gameObject.transform.parent.childCount - 1).gameObject;
+                text.SetActive(false);
+            }
+            else
+            {
+                image.sprite = ImageLoader.GetItem((int)items[i].id); // Cargar la imagen del item
+                CommonFunctions.GetChildByName(slots[i], "EmptyText").SetActive(false);
 
-                        // Ajustar la imagen al tamaño de la caja
-                        Vector2 nativeSpriteSize = image.sprite.rect.size;
-                        RectTransform auxRect = image.gameObject.GetComponent<RectTransform>();
-                        float relation = 150 / nativeSpriteSize.y; // 150 es la altura que quiero que tenga la imagen siempre
-                        Vector2 auxSizeDelta = auxRect.sizeDelta;
-                        auxRect.sizeDelta = new Vector2(nativeSpriteSize.x * relation, 150);
-                        image.gameObject.SetActive(true);
+                // Ajustar la imagen al tamaño de la caja
+                Vector2 nativeSpriteSize = image.sprite.rect.size;
+                RectTransform auxRect = image.gameObject.GetComponent<RectTransform>();
+                float relation = 150 / nativeSpriteSize.y; // 150 es la altura que quiero que tenga la imagen siempre
+                Vector2 auxSizeDelta = auxRect.sizeDelta;
+                auxRect.sizeDelta = new Vector2(nativeSpriteSize.x * relation, 150);
+                image.gameObject.SetActive(true);
 
-                        GameObject text = image.gameObject.transform.parent.GetChild(image.gameObject.transform.parent.childCount - 1).gameObject;
-                        text.GetComponent<TextMeshProUGUI>().text = items[i].itemAmount.ToString();
-                        text.SetActive(true);
-                    }
-                }                    
-            }            
+                GameObject text = CommonFunctions.GetChildByName(slots[i], "ItemAmount");
+                text.GetComponent<TextMeshProUGUI>().text = items[i].itemAmount.ToString();
+                text.SetActive(true);
+            }           
         }
     }
 
--- Assets/Scripts/InventoryScene/Canvas/InventoryPanelController.cs
+++ Assets/Scripts/InventoryScene/Canvas/InventoryPanelController.cs
@@ -52,33 +52,32 @@ public class InventoryPanelController : MonoBehaviour
     {
         GameObject slot = Instantiate(m_slotPrefab, this.transform);
         slot.GetComponent<OpenInventoryItemInfo>().m_itemIndex = index;
-        Image[] images = slot.GetComponentsInChildren<Image>(true);
-        foreach (Image image in images)
+        Image image = CommonFunctions.GetChildByName(slot, "SlotImage").GetComponent<Image>();
+        if (item != null)
         {
-            if (image.gameObject.name == "SlotImage")
-            {
-                if (item != null)
-                {
-                    image.sprite = ImageLoader.GetItem((int)item.id); // Cargar la imagen del item
-                    Vector2 nativeSpriteSize = image.sprite.rect.size;
-                    RectTransform auxRect = image.gameObject.GetComponent<RectTransform>();
-                    float relation = 175 / nativeSpriteSize.y; // 175 es la altura que quiero que tenga la imagen siempre
-                    Vector2 auxSizeDelta = auxRect.sizeDelta;
-                    auxRect.sizeDelta = new Vector2(nativeSpriteSize.x * relation, 175);
-                    image.gameObject.SetActive(true);
+            image.sprite = ImageLoader.GetItem((int)item.id); // Cargar la imagen del item
+            Vector2 nativeSpriteSize = image.sprite.rect.size;
+            RectTransform auxRect = image.gameObject.GetComponent<RectTransform>();
+            float relation = 175 / nativeSpriteSize.y; // 175 es la altura que quiero que tenga la imagen siempre
+            Vector2 auxSizeDelta = auxRect.sizeDelta;
+            auxRect.sizeDelta = new Vector2(nativeSpriteSize.x * relation, 175);
+            image.gameObject.SetActive(true);
 
-                    GameObject text = image.gameObject.transform.parent.GetChild(image.gameObject.transform.parent.childCount - 1).gameObject;
-                    text.GetComponent<TextMeshProUGUI>().text = $"x{item.itemAmount.ToString()}";
-                    text.SetActive(true);
-                }
-                else
-                {
-                    image.gameObject.SetActive(false); // En este punto tiene vacio el inventario
-                    GameObject text = image.gameObject.transform.parent.GetChild(image.gameObject.transform.parent.childCount - 1).gameObject;
-                    text.SetActive(false);
-                }
+            GameObject text = image.gameObject.transform.parent.GetChild(image.gameObject.transform.parent.childCount - 1).gameObject;
+            text.GetComponent<TextMeshProUGUI>().text = $"x{item.itemAmount.ToString()}";
+            text.SetActive(true);
 
-            }
+            GameObject emptyText = CommonFunctions.GetChildByName(slot, "EmptyText");
+            emptyText.SetActive(false);
+        }
+        else
+        {
+            image.gameObject.SetActive(false); // En este punto tiene vacio el inventario
+            GameObject text = image.gameObject.transform.parent.GetChild(image.gameObject.transform.parent.childCount - 1).gameObject;
+            text.SetActive(false);
+
+            GameObject emptyText = CommonFunctions.GetChildByName(slot, "EmptyText");
+            emptyText.SetActive(true);
         }
     }
 
